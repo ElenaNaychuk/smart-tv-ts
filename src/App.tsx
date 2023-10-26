42b800dd29614d88f08482d9d7c382cf18ef1052
Elena Naychuk <106145336+ElenaNaychuk@users.noreply.github.com>
2023-10-26 09:10:22 +0300

    paused video;

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {HomePage} from "./pages/HomePage";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import {RegistrationPage} from "./pages/RegistrationPage/RegistrationPage";
 import './App.scss';
 import videoPath from "./assets/videos/Volvo-Trucks.mp4";
 
 function App() {
+    const videoElemRef = useRef<HTMLVideoElement | null>(null);
+    const location = useLocation();
+
+    useEffect(()=>{
+        location.pathname === '/'
+            ? videoElemRef?.current?.play()
+            : videoElemRef?.current?.pause();
+    },[location.pathname])
+
   return (
     <div className="app">
         <video
+            ref={videoElemRef}
             src={videoPath}
             loop={true}
             muted={true}
